1e8b94aaef4410e17d9e64f39c81d709e087f83f
DucLA <anhduc720@gmail.com>
2017-03-23 22:42:29 +0700

    Facebook, Firebase authentication

--- FZ Social/SignInVC.swift
+++ FZ Social/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,40 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            
+            if error != nil {
+                print("Unable login with facebook")
+            }
+            else if result?.isCancelled == true {
+                print("User has canceled")
+            }
+            else {
+                print("Successfully login with facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+                
+            }
+            
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: { (result, error) in
+            if error != nil {
+                print("Unable to authenticate with FireBase")
+            } else {
+                print("Successfully authenticated with FireBase")
+            }
+        })
+        
+    }
 
 }
 
